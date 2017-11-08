615aa6ebcd7005a77cd207da1e273c49574b4987
mocivnik10 <iztok.mocivnik@poviolabs.com>
2017-11-08 13:27:53 +0100

    database settings fix

--- config/database.php
+++ config/database.php
@@ -1,13 +1,14 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
-  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-  $host = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
